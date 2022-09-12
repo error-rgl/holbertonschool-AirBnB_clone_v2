e88eeadced2f5a95d152e344a351fffd490d1201
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-09-11 21:05:04 -0500

    task2 C: Write a script that starts a Flask web application:

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module that start a Flask Webb app"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ display Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Route /c"""
+    return "C %s" % text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    """application must be listening on"""
+    app.run(host='0.0.0.0', port='5000')
